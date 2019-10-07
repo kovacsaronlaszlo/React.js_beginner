b626631b06c345f2b9abf3d58d9f84244d6cf9ca
Kovacs Áron László <kovacs.aron@arteries.hu>
2019-10-07 16:09:56 +0200

    lifecycle method

---
+++ lifecycle_methodes/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div>
+        Code goes here
+      </div>
+    )
+  };
+}
+
+export default App;
