7730948b982c146083e95b1efdb93deba8b8199c
Kovacs Áron László <kovacs.aron@arteries.hu>
2019-10-11 13:16:27 +0200

    basic lifecycle methods

--- lifecycle_methodes/src/App.js
+++ lifecycle_methodes/src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {};
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    // return the new, updated state based upon the props
+  }
+
+  getSnapshotBeforeUpdate() {
+    // creat backup of the current way thing are
+  }
+
+  // componentWillMount() {}
+
+  // only run once, when the component showing on the screen
+  componentDidMount() {
+    // GET the data I need to correctly display
+  }
+
+  // receiving props, from the parent component
+  // checking the receiving props is change or not, if it change it changes it
+  /* componentWillReceiveProps(nextProps) {
+    if (nextProps.whatever !== this.props.whatever) {
+      // do something here
+    }
+  } */
+
+  // give us to optimize our app
+  shouldComponentUpdate(nextProps, nextState) {
+    // return true if want it to update
+    // return false if not
+  }
+
+  // componentWillUpdate() {}
+
+  // before you close the component
+  componentWillUnmount() {
+    // teardown or cleanup your code before component disappear
+    // remove event listener
   }
 
   render() {
